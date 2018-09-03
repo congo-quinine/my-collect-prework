d72d54a1995e47f80c1d591a2f65326d4c2009b5
Rob Parker <into.the.parker@gmail.com>
2018-09-03 12:25:28 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,17 +1,7 @@
   def my_collect(array)
-    collect_return = []
     i=0
     while i < array.length
       yield array[i]
-      
       i += 1
     end
-    puts 'Return Value'
-    collect_return
   end
-
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-my_collect(array) do |name|
-  puts name.split(" ").first
-end
