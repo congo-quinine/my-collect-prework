089c481adb2f2ad9e5fc5f052a14bc0ad88005fe
Rob Parker <into.the.parker@gmail.com>
2018-09-03 10:38:08 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,11 @@
   def my_collect(array)
+    collection_return = []
     i=0
     while i < array.length
       yield array[i]
+      collection_return << array[i]
       i += 1
     end
-    
+    collection_return
   end
 
